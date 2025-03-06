fa2811ea52f26fc9c1594caa910b48de627d991d
Pol <pol.carnicer@estudiantat.upc.edu>
2025-03-06 19:02:26 +0100

    Inici de sesio

--- app/src/main/java/com/example/frontaire/MainActivity.kt
+++ app/src/main/java/com/example/frontaire/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.frontaire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.frontaire.Login
+import com.example.frontaire.Signup
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Login()
         }
+        setContent {
+            Signup()
+        }
     }
+
 }
+
+/*
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("login") { Login(navController) }
+        composable("signup") { Signup(navController) }
+    }
+}*/
